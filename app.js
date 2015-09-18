8ef2637c131e9fc22eaf0525e6a1acb3f344bcd0
Anit Gandhi <anitgandhi@gmail.com>
2015-09-18 20:50:43 +0100

    it works

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var spawn = require("child_process").spawn;
+var vlc = require('vlc')([
+  '-I', 'dummy',
+  '-V', 'dummy',
+  '--verbose', '1',
+  '--no-video-title-show',
+  '--no-disable-screensaver',
+  '--no-snapshot-preview',
+]);
 
-var port = 80;
+
+var port = 8080;
 
 // The public site
 app.use(express.static('public'));
-var server = app.listen(80);
+server.listen(port);
 
 // APIs and socket interactions
 
@@ -20,6 +30,21 @@ testURL = "https://www.youtube.com/watch?v=z5ZdjwbQnXc";
 var process = spawn('python',["audio_url.py", testURL]);
 process.stdout.on('data', function (data) {
 	results = JSON.parse(data);
-	console.log(results);
+
+	console.log("Audio URL: " + results.audioUrl);
+
+	var media = vlc.mediaFromUrl(results.audioURL);
+	media.parseSync();
+
+	var player = vlc.mediaplayer;
+	player.media = media;
+
+	player.play();
+
+	// pause after 10 seconds
+	setTimeout(function() {
+		player.pause();
+	}, 10000);
 })
+
 
